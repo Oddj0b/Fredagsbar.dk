d4479b97e269c648965492934e1dd12324686c35
Villars Gimm <villars.gimm@gmail.com>
2017-12-19 16:50:47 +0100

    Add XCUITest target and Carthage

---
+++ fredagsbar.dkUITests/fredagsbar_dkUITests.m
@@ -0,0 +1,39 @@
+//
+//  fredagsbar_dkUITests.m
+//  fredagsbar.dkUITests
+//
+//  Created by Villars Gimm on 19/12/2017.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fredagsbar_dkUITests : XCTestCase
+
+@end
+
+@implementation fredagsbar_dkUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
