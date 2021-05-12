fec398b5aa56d0332b92e6fa324f18a1dbacefe5
Villars Gimm <villars.gimm@gmail.com>
2021-05-12 12:59:29 +0200

    Rubocop automatic changes

--- .github/createProgramFolder/programFolder.rb
+++ .github/createProgramFolder/programFolder.rb
@@ -1,42 +1,44 @@
 #!/usr/bin/ruby
 require 'fileutils'
 
-$programID = ""
+$programID = ''
 
 def setProgramID(programID)
-  $programID = programID.gsub(/[^0-9A-Za-z]/, "_")
-  return $programID
+  $programID = programID.gsub(/[^0-9A-Za-z]/, '_')
+  $programID
 end
 
 def checkTemplateFolder(folderPath)
-  raise "No Guides and Template folder" unless File.directory?(folderPath)
+  raise 'No Guides and Template folder' unless File.directory?(folderPath)
 end
 
 def checkProgramFolder(programFolder)
-  raise "No program folder" unless File.directory?(programFolder)
+  raise 'No program folder' unless File.directory?(programFolder)
 end
 
-def createProgramFolder(programFolder,programID)
-  if Dir.exists?("#{programFolder}/#{programID}")
-    puts "Program directory already exists"
+def createProgramFolder(programFolder, programID)
+  if Dir.exist?("#{programFolder}/#{programID}")
+    puts 'Program directory already exists'
   else
     FileUtils.mkdir_p("#{programFolder}/#{programID}")
   end
 end
+
 def createGitBranch(templateFolder, programID, programFolder)
   raise "Expected three arguments only got #{ARGV.count}" unless ARGV.count == 3
-  #`export PROGRAM=#{$programID} > ~/.my-variables.sh"`
-  #`git checkout -b #{$programID}`
+
+  # `export PROGRAM=#{$programID} > ~/.my-variables.sh"`
+  # `git checkout -b #{$programID}`
   setProgramID(programID)
   checkTemplateFolder(templateFolder)
   createProgramFolder(programFolder, $programID)
   checkProgramFolder(programFolder)
   addTemplatesToFolder(templateFolder, $programID, programFolder)
-  #commitAndPush($programID, programFolder)
-  puts "#{$programID}"
+  # commitAndPush($programID, programFolder)
+  puts $programID.to_s
 end
 
-def commitAndPush(programID, programFolder)
+def commitAndPush(_programID, programFolder)
   `git add #{programFolder}/#{$programID}`
   `git commit -m "Automatically adding template files"`
   `git push -u origin #{$programID}`
@@ -44,8 +46,8 @@ end
 
 def addTemplatesToFolder(templateFolder, programID, programFolder)
   Dir["#{templateFolder}/template-*"].each do |file|
-    if File.exists?("#{programFolder}/#{programID}/#{File.basename(file)}")
-      puts "File already in folder"
+    if File.exist?("#{programFolder}/#{programID}/#{File.basename(file)}")
+      puts 'File already in folder'
     else
       FileUtils.cp(file, "#{programFolder}/#{programID}")
       puts "copied file: #{programFolder}/#{programID}: #{$?}"
