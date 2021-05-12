0751266db38ecba27ddfb25b5274f1574ebe1f19
Villars Gimm <villars.gimm@gmail.com>
2021-05-12 14:25:25 +0200

    Remove unnecessary code from ruby script

--- .github/createProgramFolder/programFolder.rb
+++ .github/createProgramFolder/programFolder.rb
@@ -1,47 +1,28 @@
 #!/usr/bin/ruby
 require 'fileutils'
 
-# $programID = ''
-
-# def setProgramID(programID)
-#   $programID = programID.gsub(/[^0-9A-Za-z]/, '_')
-#   $programID
-# end
-
 def checkTemplateFolder(folderPath)
-  raise 'No Guides and Template folder' unless File.directory?(folderPath)
+raise 'No Guides and Template folder' unless File.directory?(folderPath)
 end
 
 def checkProgramFolder(programFolder)
-  raise 'No program folder' unless File.directory?(programFolder)
+raise 'No program folder' unless File.directory?(programFolder)
 end
 
 def createProgramFolder(programFolder, programID)
-  if Dir.exist?("#{programFolder}/#{programID}")
-    puts 'Program directory already exists'
-  else
-    FileUtils.mkdir_p("#{programFolder}/#{programID}")
-  end
+if Dir.exist?("#{programFolder}/#{programID}")
+  puts 'Program directory already exists'
+else
+  FileUtils.mkdir_p("#{programFolder}/#{programID}")
+end
 end
 
 def createGitBranch(templateFolder, programID, programFolder)
-  raise "Expected three arguments only got #{ARGV.count}" unless ARGV.count == 3
-
-  # `export PROGRAM=#{$programID} > ~/.my-variables.sh"`
-  # `git checkout -b #{$programID}`
-  # setProgramID(programID)
-  checkTemplateFolder(templateFolder)
-  createProgramFolder(programFolder, programID)
-  checkProgramFolder(programFolder)
-  addTemplatesToFolder(templateFolder, programID, programFolder)
-  # commitAndPush($programID, programFolder)
-  # puts $programID.to_s
-end
-
-def commitAndPush(programID, programFolder)
-  `git add #{programFolder}/#{programID}`
-  `git commit -m "Automatically adding template files"`
-  `git push -u origin #{$programID}`
+raise "Expected three arguments only got #{ARGV.count}" unless ARGV.count == 3
+checkTemplateFolder(templateFolder)
+createProgramFolder(programFolder, programID)
+checkProgramFolder(programFolder)
+addTemplatesToFolder(templateFolder, programID, programFolder)
 end
 
 def addTemplatesToFolder(templateFolder, programID, programFolder)
